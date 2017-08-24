98acba4f6357801f8217d01e26f99c7d4c1f3daf
zhou_wanli <zhou_wanli@wuxiapptec.com>
2017-08-24 23:04:27 +0800

    first commit

---
+++ src/main/java/com/flower/intellij/IntellijApplication.java
@@ -0,0 +1,12 @@
+package com.flower.intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijApplication.class, args);
+	}
+}
