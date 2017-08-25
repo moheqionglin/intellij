ecd6ca40f5c5dec36c33bc7224e47a3acaf48ace
Wang Gang <wanggang1987@live.cn>
2017-08-25 15:39:54 +0800

    add tuya client

---
+++ src/main/java/com/flower/intellij/client/TuyaClient.java
@@ -0,0 +1,15 @@
+package com.flower.intellij.client;
+
+import com.tuya.smart.TuyaCloudClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TuyaClient {
+
+    private TuyaCloudClient tuyaCloudClient;
+
+    public TuyaClient() {
+        
+    }
+
+}
